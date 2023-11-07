55949c2f018c327a9c431d48749d33c5fd799864
sagarsood31 <sagarsood31@gmail.com>
2023-11-07 13:13:40 +0530

    Chat Bot 1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const OpenAI = require("openai");
 const app = express();
-app.use(express.json()); // Add this line to enable JSON request body parsing
+app.use(express.json());
 
 const openai = new OpenAI({
   apiKey: "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"
@@ -11,11 +11,11 @@ app.get('/getResponse', async (req, res) => {
   const userPrompt = req.body.userPrompt;
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
-    messages: [{"role": "user", "content": userPrompt}], // Use an array of objects
+    messages: [{"role": "user", "content": userPrompt}], 
     max_tokens: 100
   });
-  console.log(response.choices[0].message.content); // Fixed this line
-  res.send(response.choices[0].message.content); // Fixed this line
+  console.log(response.choices[0].message.content); 
+  res.send(response.choices[0].message.content); 
 });
 
 app.listen(9131, () => {
